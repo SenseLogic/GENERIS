f04499a9356fb6a5a6194a1853898c8a8dedd214
senselogic <ecstatic.coder@gmail.com>
2019-11-17 16:47:46 +0100

    % license

--- generis.d
+++ generis.d
@@ -1,21 +1,21 @@
 /*
-    This file is part of the generis distribution.
+    This file is part of the Generis distribution.
 
-    https://github.com/senselogic/generis
+    https://github.com/senselogic/GENERIS
 
     Copyright (C) 2017 Eric Pelzer (ecstatic.coder@gmail.com)
 
-    generis is free software: you can redistribute it and/or modify
+    Generis is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, version 3.
 
-    generis is distributed in the hope that it will be useful,
+    Generis is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with generis.  If not, see <http://www.gnu.org/licenses/>.
+    along with Generis.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 // -- IMPORTS
